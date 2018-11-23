1ccb4a8e5c29880f681e94f377dbdefae22f3a59
xbobci02 <43862449+xbobci02@users.noreply.github.com>
2018-11-23 20:24:07 +0100

    Trash deleting in src folder

---
+++ src/translation.c
@@ -0,0 +1,13 @@
+#ifndef TRANSLATION_IFJ
+#define TRANSLATION_IFJ
+
+void ADDToAssembler();
+void SUBToAssembler();
+void MULToAssembler();
+void DIVToAssembler();
+void IFToAssembler();
+void WHILEToAssembler();
+void FUNToAssembler();
+
+
+#endif  // TRANSLATION_IFJ
